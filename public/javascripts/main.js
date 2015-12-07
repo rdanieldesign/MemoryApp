6a116c0b718fde66b65fde09a7c1a02f25ddbfea
rdanieldesign <rdanieldesign@gmail.com>
2015-12-07 17:58:44 -0500

    Hook up db

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,35 @@
 // main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
+
+var FirstParagraph = React.createClass({
+    getData: function(){
+        var self = this;
+        $.ajax({
+            url: '/records/all',
+            dataType: 'json',
+            success: function(data){
+                self.setState({data: data[0].hello});
+            }
+        });
+    },
+    getInitialState: function(){
+        return {data: []}
+    },
+    componentDidMount: function(){
+        this.getData();
+    },
+    render: function(){
+        return (
+                <p className="firstParagraph" data={this.state.data}> {this.state.data} {this.props.children}</p>
+        );
+    }
+});
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('content')
+    <div className="wrapper">
+        <FirstParagraph test="hello" url="/records/all"> World! </FirstParagraph>
+    </div>,
+    document.getElementById('content')
 );
-
-console.log('hello');
